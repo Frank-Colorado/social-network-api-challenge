b989dcff43c9237d4fd7512746c52d0f69dc4ac8
Frank-Colorado <chawnkiee@gmail.com>
2023-07-16 23:08:10 -0400

    added routing for user

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+} = require("../../../controllers/userController");
+
+// /api/users
+router.route("/").get(getAllUsers).post(createUser);
+
+// /api/users/:id
+router.route("/:id").get(getUser);
 
 module.exports = router;
