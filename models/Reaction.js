47820d2d8544a8d8870cd469d906d84238c9ec16
Frank-Colorado <chawnkiee@gmail.com>
2023-07-16 22:32:45 -0400

    created Reaction Schema in Reaction.js file

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema } = require("mongoose");
+
+const ReactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: [true, "Reaction is required"],
+    maxlength: [280, "Reaction must be less than 280 characters"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+});
+
+module.exports = ReactionSchema;
