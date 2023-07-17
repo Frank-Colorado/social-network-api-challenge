96672e3bd424499bb72b02251fd2d2df1c86f334
Frank-Colorado <chawnkiee@gmail.com>
2023-07-16 23:43:38 -0400

    created getThought function in Thought Controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,3 +8,16 @@ const getAllThoughts = async (req, res) => {
     res.status(500).json({ err });
   }
 };
+
+const getThought = async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.id);
+    if (!thought) {
+      res.status(404).json({ message: "No thought found with this id." });
+      return;
+    }
+    res.json(thought);
+  } catch ({ err }) {
+    res.status(500).json({ err });
+  }
+};
