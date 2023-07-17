6c55a9999bdd1fe38e838d88216e45838431fb85
Frank-Colorado <chawnkiee@gmail.com>
2023-07-16 23:48:06 -0400

    created full CRUD routes for thoughtRoutes

--- routes/apiRoutes/thoughtRoutes/index.js
+++ routes/apiRoutes/thoughtRoutes/index.js
@@ -1,3 +1,21 @@
 const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require("../../../controllers/thoughtController");
+
+// /api/thoughts
+router.route("/").get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:id
+router.route("/:id").get(getThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:id/reactions
+router.route("/:id/reactions").post(addReaction).delete(deleteReaction);
 
 module.exports = router;
