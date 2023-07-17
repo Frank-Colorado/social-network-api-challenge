2bdb90d7ce6603dc59ef7420e601def00560f75b
Frank-Colorado <chawnkiee@gmail.com>
2023-07-17 16:58:46 -0400

    created config folder with connection.js file

--- server.js
+++ server.js
@@ -8,17 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(routes);
-
-mongoose
-  .connect("mongodb://127.0.0.1/socialNetworkDB")
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB", err);
-  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
