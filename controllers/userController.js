57bbc4a65f5c53de09acbf72a9ea4c01a7e91cd4
Frank-Colorado <chawnkiee@gmail.com>
2023-07-16 23:04:29 -0400

    added getUser function in User controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,3 +8,26 @@ const getAllUsers = async (req, res) => {
     res.status(500).json({ err });
   }
 };
+
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).populate(
+      {
+        path: "thoughtsId",
+        select: "-__v",
+      },
+      {
+        path: "friends",
+        select: "-__v -email -thoughtsId -friends",
+      }
+    );
+    if (!user) {
+      res.status(404).json({ message: "No user found with this id." });
+      return;
+    }
+    const userFriends = user.friendCount;
+    res.json({ user, userFriends });
+  } catch ({ err }) {
+    res.status(500).json({ err });
+  }
+};
