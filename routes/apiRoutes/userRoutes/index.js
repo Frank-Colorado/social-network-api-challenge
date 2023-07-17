17ab4856a8d2001655113fd45131adf2e1416d71
Frank-Colorado <chawnkiee@gmail.com>
2023-07-16 23:37:31 -0400

    added all CRUD routes for User Routes

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -3,12 +3,19 @@ const {
   getAllUsers,
   getUser,
   createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
 } = require("../../../controllers/userController");
 
 // /api/users
 router.route("/").get(getAllUsers).post(createUser);
 
 // /api/users/:id
-router.route("/:id").get(getUser);
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
