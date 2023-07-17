2e65c2a3af673db3d0bc483ba1cbb750d35d1b7a
Frank-Colorado <chawnkiee@gmail.com>
2023-07-17 18:45:07 -0400

    added comments to controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,45 +1,63 @@
 const { Thought, User } = require("../models");
 
-const getAllThoughts = async (req, res) => {
+// This is a controller function that will be called when the route '/api/thoughts' is hit with a GET request
+const getAllThoughts = async (_req, res) => {
   try {
+    // We find all the thoughts in the database
     const thoughts = await Thought.find({});
+    // We return the thoughts as JSON
     res.json(thoughts);
   } catch (err) {
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/thoughts/:id' is hit with a GET request
 const getThought = async (req, res) => {
   try {
+    // We find the thought by its ID in the database using the req.params.id
     const thought = await Thought.findById(req.params.id);
+    // If the thought does not exist
     if (!thought) {
+      // We return an error message
       res.status(404).json({ message: "No thought found with this id." });
       return;
     }
+    // Otherwise we return the thought as JSON
     res.json(thought);
   } catch (err) {
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/thoughts' is hit with a POST request
 const createThought = async (req, res) => {
+  // We destructure the thoughtText and username from the request body
   const { thoughtText, username } = req.body;
   try {
+    // We find the user by their username in the database
     const user = await User.findOne({ username: username });
+    // If the user does not exist
     if (!user) {
+      // We return an error message
       res.status(404).json({ message: "No user found with this username." });
       return;
     }
+    // Otherwise we create a thought using the thoughtText and username
     const thought = await Thought.create({ thoughtText, username });
+    // We return the thought as JSON
     res.json(thought);
   } catch (err) {
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/thoughts/:id' is hit with a PUT request
 const updateThought = async (req, res) => {
+  // We destructure the thoughtText from the request body
   const { thoughtText } = req.body;
   try {
+    // We find the thought by its ID in the database and update it with the thoughtText
     const thought = await Thought.findByIdAndUpdate(
       req.params.id,
       {
@@ -47,10 +65,13 @@ const updateThought = async (req, res) => {
       },
       { new: true }
     );
+    // If the thought does not exist
     if (!thought) {
+      // We return an error message
       res.status(404).json({ message: "No thought found with this id." });
       return;
     }
+    // Otherwise we return the thought as JSON
     res.json(thought);
   } catch (err) {
     console.log(err);
@@ -58,36 +79,50 @@ const updateThought = async (req, res) => {
   }
 };
 
+// This is a controller function that will be called when the route '/api/thoughts/:id' is hit with a DELETE request
 const deleteThought = async (req, res) => {
   try {
+    // We find the thought by its ID in the database and delete it
     const thought = await Thought.findByIdAndDelete(req.params.id);
+    // If the thought does not exist
     if (!thought) {
+      // We return an error message
       res.status(404).json({ message: "No thought found with this id." });
       return;
     }
+    // Otherwise we return the thought as JSON
     res.json(thought);
   } catch (err) {
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/thoughts/:id/reactions' is hit with a POST request
 const addReaction = async (req, res) => {
+  // We destructure the reactionBody and username from the request body
   const { reactionBody, username } = req.body;
   try {
+    // We find the user by their username in the database
     const user = await User.findOne({ username: username });
+    // If the user does not exist
     if (!user) {
+      // We return an error message
       res.status(404).json({ message: "No user found with this username." });
       return;
     }
+    // Otherwise we find the thought by its ID in the database and update the reactions array with the reactionBody and username
     const thought = await Thought.findByIdAndUpdate(
       req.params.id,
       { $push: { reactions: { reactionBody, username } } },
       { new: true }
     );
+    // If the thought does not exist
     if (!thought) {
+      // We return an error message
       res.status(404).json({ message: "No thought found with this id." });
       return;
     }
+    // Otherwise we return the thought as JSON
     res.json(thought);
   } catch (err) {
     console.log(err);
@@ -95,18 +130,24 @@ const addReaction = async (req, res) => {
   }
 };
 
+// This is a controller function that will be called when the route '/api/thoughts/:thoughtId/reactions/:reactionId' is hit with a DELETE request
 const deleteReaction = async (req, res) => {
+  // We destructure the thoughtId and reactionId from the request params
   const { thoughtId, reactionId } = req.params;
   try {
+    // We find the thought by its ID in the database and update the reactions array by removing the reaction with the reactionId
     const thought = await Thought.findByIdAndUpdate(
       thoughtId,
       { $pull: { reactions: { _id: reactionId } } },
       { new: true }
     );
+    // If the thought does not exist
     if (!thought) {
+      // We return an error message
       res.status(404).json({ message: "No thought found with this id." });
       return;
     }
+    // Otherwise we return the thought as JSON
     res.json(thought);
   } catch (err) {
     res.status(500).json({ err });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,65 @@
 const { User } = require("../models");
 
-const getAllUsers = async (req, res) => {
+// This is a controller function that will be called when the route '/api/users' is hit with a GET request
+const getAllUsers = async (_req, res) => {
   try {
+    // We find all the users in the database
     const users = await User.find({});
+    // We return the users as JSON
     res.json(users);
   } catch (err) {
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/users/:id' is hit with a GET request
 const getUser = async (req, res) => {
   try {
+    // We find the user by its ID in the database using the req.params.id
     const user = await User.findById(req.params.id)
+      // We populate the thoughtsId field with the thoughtText, createdAt, and reactions fields
       .populate({
         path: "thoughtsId",
         select: "thoughtText createdAt reactions",
       })
+      // We populate the friends field with the username field
       .populate({
         path: "friends",
         select: "username",
       });
-
+    // If the user does not exist
     if (!user) {
+      // We return an error message
       res.status(404).json({ message: "No user found with this id." });
       return;
     }
+    // Otherwise we return the user as JSON
     res.json(user);
   } catch (err) {
-    console.log({ err });
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/users' is hit with a POST request
 const createUser = async (req, res) => {
+  // We destructure the username and email from the request body
   const { username, email } = req.body;
   try {
+    // We create a user using the username and email
     const user = await User.create({ username, email });
+    // We return the user as JSON
     res.json(user);
   } catch (err) {
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/users/:id' is hit with a PUT request
 const updateUser = async (req, res) => {
+  // We destructure the username and email from the request body
   const { username, email } = req.body;
   try {
+    // We find the user by its ID in the database and update it with the username and email
     const user = await User.findByIdAndUpdate(
       req.params.id,
       {
@@ -53,60 +68,79 @@ const updateUser = async (req, res) => {
       },
       { new: true }
     );
+    // If the user does not exist
     if (!user) {
+      // We return an error message
       res.status(404).json({ message: "No user found with this id." });
       return;
     }
+    // Otherwise we return the user as JSON
     res.json(user);
   } catch (err) {
-    console.log({ err });
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/users/:id' is hit with a DELETE request
 const deleteUser = async (req, res) => {
   try {
+    // We find the user by its ID in the database and delete it
     const user = await User.findByIdAndDelete(req.params.id);
+    // If the user does not exist
     if (!user) {
+      // We return an error message
       res.status(404).json({ message: "No user found with this id." });
       return;
     }
+    // Otherwise we return a message saying the user was deleted
     res.json({ message: "User deleted." });
   } catch (err) {
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/users/:userId/friends/:friendId' is hit with a POST request
 const addFriend = async (req, res) => {
+  // We destructure the userId and friendId from the request params
   const { userId, friendId } = req.params;
   try {
+    // We find the user by its ID in the database and update the friends array with the friendId
     const user = await User.findByIdAndUpdate(
       userId,
       { $addToSet: { friends: friendId } },
       { new: true }
     );
+    // If the user does not exist
     if (!user) {
+      // We return an error message
       res.status(404).json({ message: "No user found with this id." });
       return;
     }
+    // Otherwise we return the user as JSON
     res.json(user);
   } catch (err) {
     res.status(500).json({ err });
   }
 };
 
+// This is a controller function that will be called when the route '/api/users/:userId/friends/:friendId' is hit with a DELETE request
 const removeFriend = async (req, res) => {
+  // We destructure the userId and friendId from the request params
   const { userId, friendId } = req.params;
   try {
+    // We find the user by its ID in the database and update the friends array by removing the friendId
     const user = await User.findByIdAndUpdate(
       userId,
       { $pull: { friends: friendId } },
       { new: true }
     );
+    // If the user does not exist
     if (!user) {
+      // We return an error message
       res.status(404).json({ message: "No user found with this id." });
       return;
     }
+    // Otherwise we return the user as JSON
     res.json(user);
   } catch (err) {
     res.status(500).json({ err });
